625dccf844b37a06dae1db7620894983e63c6151
cascade-release-local <cascade-release-local@users.noreply.github.com>
2026-05-05 12:06:33 -0700

    Fix cascade cask release URL

--- Casks/cascade.rb
+++ Casks/cascade.rb
@@ -2,7 +2,7 @@ cask "cascade" do
   version "0.2.1"
   sha256 "c17cae83ba20abcfd175dd8c68780d378dd1f9f5fd3ea45470b96b9c46c91135"
 
-  url "https://github.com/zacharyfmarion/cascade/releases/download/v\#{version}/Cascade_\#{version}_aarch64.dmg",
+  url "https://github.com/zacharyfmarion/cascade/releases/download/v0.2.1/Cascade_0.2.1_aarch64.dmg",
       verified: "github.com/zacharyfmarion/"
 
   name "Cascade"
